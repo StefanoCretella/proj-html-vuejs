d9c3e338e8994d46ee5ea4ad060bc168d2a3be7a
federicoalagna <federicoalagna01@gmail.com>
2024-06-11 13:13:26 +0200

    [UPD] Header Homepage

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,8 +9,15 @@ export default{
 
 <template>
     <h2>sono il footer</h2>
+    <ul>
+        <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'contact'}">Contact</router-link>
+        </li>
+    </ul>
+
 </template>
 
 <style scoped lang="scss">
+
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,20 +8,93 @@ export default{
 </script>
 
 <template>
-    <h2>sono l'header</h2>
-    <ul>
-        <li>
-            <router-link :to="{name:'home'}">Home</router-link>
+
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+    <div class="container px-3 my-3">
+      <a class="navbar-brand" href="#"><img decoding="async" width="231" height="44" src="https://avada.website/marketing-consultant/wp-content/uploads/sites/142/2021/04/avada-marketing-consultant-logo.svg" alt="" class="img-responsive wp-image-2746 disable-lazyload"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <router-link class="nav-link fw-medium fs-6" :to="{name:'home'}">Home</router-link>
         </li>
-        <li>
-            <router-link :to="{name:'about'}">About us</router-link>
+        <li class="nav-item">
+            <a class="nav-link fw-medium fs-6" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-medium fs-6" href="#">Why Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-medium fs-6" href="#">Case Studies</a>
+          </li>
+        <li class="nav-item">
+            <router-link class="nav-link fw-medium fs-6" :to="{name:'about'}">About</router-link>
         </li>
-        <li>
-            <router-link :to="{name:'contact'}">Contact</router-link>
-        </li>
-    </ul>
+  
+   
+  
+          <li class="nav-item">
+            <a class="nav-link fw-medium fs-6" href="#">Blog</a>
+          </li>
+        </ul>
+        <span class="navbar-text fw-medium fs-6 ms-3">
+          <i class="fas fa-phone icon-orange"></i>(555) 802-1234
+        </span>
+        <a class="btn btn-custom ms-3" href="#">Free Quote</a>
+      </div>
+    </div>
+  </nav>
+
 </template>
 
 <style scoped lang="scss">
+    .navbar-custom {
+      background-color: #ffffff; 
+      border-bottom: 1px solid #e7e7e7; 
+    
+    }
+    .navbar-custom .navbar-brand {
+      font-size: 1.75rem; 
+      font-weight: bold; 
+      color: #333; 
+    }
+    .navbar-custom .navbar-nav .nav-link,
+    .navbar-custom .navbar-nav .btn-custom,
+    .navbar-custom .navbar-text {
+      font-size: 1rem; 
+      margin-right: 2rem; 
+      color: #333; 
+    }
+    .navbar-custom .navbar-nav .nav-link:hover,
+    .navbar-custom .navbar-nav .nav-link:focus,
+    .navbar-custom .navbar-nav .btn-custom:hover,
+    .navbar-custom .navbar-nav .btn-custom:focus {
+      color: #ff6600;
+    }
+    .navbar-custom .btn-custom {
+      background-color: #ff660016;
+      color: #ff6600;
+      border-radius: 10px;
+      padding: 1rem 1.5rem; 
+      font-size: 1rem; 
 
+    }
+    .navbar-custom .btn-custom:hover,
+    .navbar-custom .btn-custom:focus {
+      background-color: #ff6600;
+      color: #e7e7e7;
+    }
+    .navbar-custom .navbar-text i {
+      margin-right: 10px; 
+    }
+    .icon-orange {
+  color: #ff6600; 
+}
+.chevron-icon {
+  display: inline-block;
+ 
+  vertical-align: middle; 
+}
 </style>
