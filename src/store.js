46244f29712403dc63f206ae7f17f164cf48e60a
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-06-12 02:28:42 +0200

    [UPD] images in home component repeat

--- src/store.js
+++ src/store.js
@@ -19,7 +19,7 @@ export const store=reactive({
     componentRepeatHome:[
         {
             id:0,
-            images:[],
+            images:['../assets/img/download.png','../assets/img/download1.png','../assets/img/download3.png','../assets/img/download2.png'],
             logoText:{id:0, logo:'', text:'Business Growth'},
             title:'Increase Brand Awareness',
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae earum atque, assumenda officia ducimus vero.',
@@ -27,7 +27,7 @@ export const store=reactive({
         },
         {
             id:1,
-            images:[],
+            images:['../assets/img/download4.png','../assets/img/download5.png','../assets/img/download6.png','../assets/img/download7.png'],
             logoText:{id:1, logo:'', text:'Investors in People'},
             title:'In-House Sales Training',
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae earum atque, assumenda officia ducimus vero.',
@@ -35,7 +35,7 @@ export const store=reactive({
         },
         {
             id:2,
-            images:[],
+            images:['../assets/img/download8.png','../assets/img/download9.png','../assets/img/download10.png','../assets/img/download11.png','../assets/img/download12.png'],
             logoText:{id:2, logo:'',text:'Social Media Analysis'},
             title:'Harness Your Social Proof',
             text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae earum atque, assumenda officia ducimus vero.',
